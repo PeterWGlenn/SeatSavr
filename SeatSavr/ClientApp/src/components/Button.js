1dc18dac9cd50a75259fb3a851e12a4093f28357
ASimpleStudent <60008731+ASimpleStudent@users.noreply.github.com>
2021-02-28 20:44:37 -0500

    Created a Button

---
+++ SeatSavr/ClientApp/src/components/Button.js
@@ -0,0 +1,13 @@
+﻿import React, { useState } from 'react'
+
+function Button(props) {
+    const [size] = useState(props.size)
+    const [color] = useState(props.color)
+    const [content] = useState(props.content)
+    return (
+        <button style={{ backgroundColor: color, fontSize: size }} > { content } </button>
+        )
+
+}
+
+export default Button;
