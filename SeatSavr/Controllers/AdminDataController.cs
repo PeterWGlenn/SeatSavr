0ab79e9d7fd39896a188e1d87a789ea1cdcb5b0f
Peter Glenn <peter.glenn.17@cnu.edu>
2021-03-04 14:00:10 -0500

    Created Database.cs to handle database interaction. Added Sqlite Core to project references. Added Admin backend class. Created FetchAdminData component to test database interaction.

---
+++ SeatSavr/Controllers/AdminDataController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeatSavr.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminDataController : ControllerBase
+    {
+        private int _idInc = 23;
+
+        [HttpGet]
+        public IEnumerable<Admin> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 50).Select(index => new Admin
+            {
+                // TODO PG -> Made this use Database.cs instead of using randomly generated data
+                Id = _idInc++
+            })
+            .ToArray();
+        }
+    }
+}
