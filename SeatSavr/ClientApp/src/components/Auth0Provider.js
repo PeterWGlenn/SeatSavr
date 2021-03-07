e287c707f2de1448cf96f6d9a5720d30b92852e9
ASimpleStudent <60008731+ASimpleStudent@users.noreply.github.com>
2021-03-06 22:19:03 -0500

    2nd Try. Following Quick start guide on Auth0

---
+++ SeatSavr/ClientApp/src/components/Auth0Provider.js
@@ -0,0 +1,29 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.render(
+    <Auth0Provider
+        domain="dev-h03x6nz6.us.auth0.com"
+        clientId="5nsvjGuVj765f50rW6tBPQevHdM1aP9C"
+        redirectUri={window.location.origin}
+    >
+        <App />
+    </Auth0Provider>,
+    document.getElementById("root")
+); import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.render(
+    <Auth0Provider
+        domain="dev-h03x6nz6.us.auth0.com"
+        clientId="5nsvjGuVj765f50rW6tBPQevHdM1aP9C"
+        redirectUri={window.location.origin}
+    >
+        <App />
+    </Auth0Provider>,
+    document.getElementById("root")
+);
--- SeatSavr/ClientApp/src/components/NavMenu.js
+++ SeatSavr/ClientApp/src/components/NavMenu.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
-import Button from './Button'
+import LoginButton from './login-button'
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
