d8b23d0a242a3ceae85ea2b9134df5174effee08
ASimpleStudent <60008731+ASimpleStudent@users.noreply.github.com>
2021-03-08 14:32:20 -0500

    Revert "Created Database.cs to handle database interaction. Added Sqlite Core to project references. Added Admin backend class. Created FetchAdminData component to test database interaction."
    This reverts commit 0ab79e9d7fd39896a188e1d87a789ea1cdcb5b0f.

--- SeatSavr/ClientApp/src/App.js
+++ SeatSavr/ClientApp/src/App.js
@@ -4,7 +4,6 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
-import { FetchAdminData } from './components/FetchAdminData';
 
 import './custom.css'
 
@@ -17,7 +16,6 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
-        <Route path='/fetch-admin-data' component={FetchAdminData} />
       </Layout>
     );
   }
