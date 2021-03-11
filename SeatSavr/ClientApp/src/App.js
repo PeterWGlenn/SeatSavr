5ec590219828bfaac52e7b9f8979bd21cd024a0a
ASimpleStudent <60008731+ASimpleStudent@users.noreply.github.com>
2021-03-11 02:34:55 -0500

    Working on Endless looping through access token requests that end up killing the webapp

--- SeatSavr/ClientApp/src/App.js
+++ SeatSavr/ClientApp/src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+import { Switch, Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { SelectArea } from './components/SelectArea';
 import { FetchAdminData } from './components/FetchAdminData';
 
 import './custom.css'
+import AuthService from './AuthService';
 
 export default class App extends Component {
   static displayName = App.name;
+    constructor() {
+        super();
+        this.authService = new AuthService();
+    }
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/select-area' component={SelectArea} />
-        <Route path='/fetch-admin-data' component={FetchAdminData} />
+    createLogoutButton() {
+        let button = null;
+        if (this.authService.isAuthenticated()) {
+            button = <button onClick={() => this.authService.logout()}>Logout</button>;
+        }
+        return button;
+    }
+    renderHome() {
+        let resultComponent = <Home auth={this.authService} />;
+
+        if (!this.authService.isAuthenticated()) {
+            this.authService.login();
+            resultComponent = <div><p>Redirecting to the authentication service...</p></div>
+        }
+        return resultComponent;
+    }
+
+    startSession(history) {
+        this.authService.handleAuthentication(history);
+        return <div><p>Starting session...</p></div>;
+    }
+    render() {
+        this.authService.login();
+        let logoutButton = this.createLogoutButton();
+
+     return (
+         <Layout>
+             <header className="App-header">
+                 {logoutButton}
+                 <h1 className="App-title">SeatSavr</h1>
+             </header>
+             <Switch>
+                 <Route exact path='/' component={Home} />
+                 <Route path='/select-area' component={SelectArea} />
+                 <Route path='/fetch-admin-data' component={FetchAdminData} />
+             </Switch>
       </Layout>
     );
   }
--- SeatSavr/ClientApp/src/index.js
+++ SeatSavr/ClientApp/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history"
+
 import { Auth0Provider } from "@auth0/auth0-react"
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
@@ -12,9 +12,9 @@ const rootElement = document.getElementById('root');
 
 ReactDOM.render(
     <BrowserRouter basename={baseUrl}>
-        <Auth0Provider>
+        
             <App />
-        </Auth0Provider>
+        
   </BrowserRouter>,
   rootElement);
 
