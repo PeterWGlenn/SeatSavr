92f23cbcda1aae4753059595f8ecced35943b987
ASimpleStudent <60008731+ASimpleStudent@users.noreply.github.com>
2021-03-15 15:42:10 -0400

    Began Work on Editor Layout: Working through uploading an image

---
+++ SeatSavr/ClientApp/src/components/EditorLayout.js
@@ -0,0 +1,43 @@
+﻿
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Snackbar from '@material-ui/core/Snackbar';
+import ImageUploader from 'react-images-upload';
+import './EditorLayout.css'
+
+
+
+
+export class EditorLayout extends Component {
+    static displayName = EditorLayout.name;
+    static areaRadius = 12;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            pictures: []
+        }
+        this.onDrop = this.onDrop.bind(this);
+    }
+
+    onDrop(picture) {
+        this.setState({
+            pictures: this.state.pictures.concat(picture),
+        });
+    }
+
+    render() {
+        return (
+            <ImageUploader
+                withIcon={true}
+                buttonText='Choose images'
+                onChange={this.onDrop}
+                imgExtension={['.jpg', '.gif', '.png']}
+                maxFileSize={5242880}
+            />
+        );
+
+    }
+              
+}
