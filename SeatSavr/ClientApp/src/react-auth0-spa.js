8a5f8bbca857ef5280c7d99d997604d36662d159
ASimpleStudent <60008731+ASimpleStudent@users.noreply.github.com>
2021-03-25 10:18:18 -0400

    Changes to fix deployment issues

--- SeatSavr/ClientApp/src/react-auth0-spa.js
+++ SeatSavr/ClientApp/src/react-auth0-spa.js
@@ -1,8 +1,8 @@
 ﻿import React, { useState, useEffect, useContext } from 'react';
 import createAuth0Client from '@auth0/auth0-spa-js';
 
-const DEFAULT_REDIRECT_CALLBACK = () =>
-    window.history.replaceState({}, document.title, window.location.pathname);
+ //const DEFAULT_REDIRECT_CALLBACK = () =>
+ //   window.history.replaceState({}, document.title, window.location.pathname);
 
 export const Auth0Context = React.createContext();
 export const useAuth0 = () => useContext(Auth0Context);
