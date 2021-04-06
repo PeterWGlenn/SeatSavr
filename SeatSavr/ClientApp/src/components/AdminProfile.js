4483f31847e379f52c6862eef96a1c89d74578de
ASimpleStudent <60008731+ASimpleStudent@users.noreply.github.com>
2021-04-05 21:59:42 -0400

    Fixing database errors by including a picture in the AdminProfile page.

--- SeatSavr/ClientApp/src/components/AdminProfile.js
+++ SeatSavr/ClientApp/src/components/AdminProfile.js
@@ -9,23 +9,16 @@ class AdminProfile extends React.Component {
         return (
             <div className="container">
                 <div className="main-body">
-                    {/* Breadcrumb */}
-                    <nav aria-label="breadcrumb" className="main-breadcrumb">
-                        <ol className="breadcrumb">
-                            <li className="breadcrumb-item"><a href="index.html">Home</a></li>
-                            <li className="breadcrumb-item"><a href="javascript:void(0)">User</a></li>
-                            <li className="breadcrumb-item active" aria-current="page">User Profile</li>
-                        </ol>
-                    </nav>
+                   
                     {/* /Breadcrumb */}
                     <div className="row gutters-sm">
                         <div className="col-md-4 mb-3">
                             <div className="card">
                                 <div className="card-body">
                                     <div className="d-flex flex-column align-items-center text-center">
-                                        <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width={150} />
+                                        <img src={picture} alt="Admin" className="rounded-circle" width={150} />
                                         <div className="mt-3">
-                                            <h4>John Doe</h4>
+                                            <h4>{name}</h4>
                                             <p className="text-secondary mb-1">Full Stack Developer</p>
                                             <p className="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
                                             <button className="btn btn-primary">Follow</button>
